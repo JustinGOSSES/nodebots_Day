6e05ecc3eea365817e85ace5f76021069fc1b6d2
Justin Gosses <Justin.C.Gosses@nasa.gov>
2016-10-19 22:59:49 -0500

    A file was deleted

--- led-blink.js
+++
@@ -1,9 +0,0 @@
-var five = require("johnny-five");
-var Tessel = require("tessel-io");
-
-var board = new five.Board( { io: new Tessel() });
-
-board.on("ready", function() {
-  var led = new five.Led("a5");
-  led.blink();
-});
